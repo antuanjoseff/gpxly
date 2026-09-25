de0c45991e4cc4ebf7fae605a7b30a1761e6916d
toni <toni.hernandez@udg.edu>
2026-09-25 13:00:40 +0200

    offline basemap with foutains and streets. Coastline not drawn

--- scripts/panetiler.sh
+++ scripts/panetiler.sh
@@ -1,2 +1,9 @@
 # Windows
 java -Xmx4g -jar planetiler.jar planetiler_map.yaml --osm=data/sources/local.osm.pbf --output=data/output.pmtiles
+
+# linux
+java -Xmx8g -jar planetiler.jar generate-custom   --schema=chatgpt.yaml   --osm-path=catalunya.pbf   --output=catalunya.mbtiles   --force
+
+java -Xmx8g -jar planetiler.jar generate-custom   --schema=chatgpt2.yaml   --osm-path=catalunya.pbf   --output=catalunya2.mbtiles   --force
+
+java -Xmx8g -jar planetiler.jar generate-custom   --schema=chatgpt5.yaml   --osm-path=catalunya.pbf   --output=catalunya5.mbtiles   --force
--- scripts/planetiler.sh
+++ scripts/planetiler.sh
@@ -1,2 +1,6 @@
 # linux
 java -jar planetiler.jar planetiler_map.yaml --osm_path=catalunya.pbf --output=catalunya.mbtiles
+
+java -Xmx4g -jar planetiler.jar planetiler.yaml \
+  --osm-path=/home/toni/git/mapes_offline/catalunya.pbf \
+  --output=/home/toni/git/mapes_offline/catalunya.mbtiles
